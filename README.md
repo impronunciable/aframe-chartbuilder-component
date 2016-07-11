c9e6749f60f59253ef4e1af3261795cc21c2b5dc
Dan Zajdband <dan.zajdband@gmail.com>
2016-07-11 18:19:26 -0400

    Added readme

--- README.md
+++ README.md
@@ -1,39 +1,23 @@
-## aframe-component-boilerplate
+## aframe-chartbuilder-component
 
-> This is not at all means required for writing an A-Frame component. It is intended for publishing and sharing a component for the community to re-use.
+A component for using [ChartBuilder](http://quartz.github.io/Chartbuilder) charts in AFrame.
 
-Boilerplate for sharing [A-Frame](https://aframe.io) [components](https://aframe.io/docs/core/component.html):
+Here is a [basic example](http://impronunciable.github.io/aframe-chartbuilder-component/examples/basic)
 
-1. `npm install && npm run unboil` to rename and trim stuff. Don't forget to **unboil**.
-2. [Write your component](http://ngokevin.com/blog/aframe-component).
-3. Build examples (`npm run dev` to watch for changes to build example bundles).
-4. `npm publish` and commit the `dist/` files.
-5. `npm run ghpages` to share with people.
-6. Share on [Slack](https://aframevr-slack.herokuapp.com/) and [awesome-aframe](https://github.com/aframevr/awesome-aframe)!
-
-Examples:
-
-- [aframe-layout-component](https://github.com/ngokevin/aframe-layout-component)
-- [aframe-text-component](https://github.com/ngokevin/aframe-text-component)
-- [aframe-extrude-and-lathe](https://github.com/JosePedroDias/aframe-extrude-and-lathe)
-- [aframe-obj-loader-component](https://github.com/donmccurdy/aframe-obj-loader-component)
-- [aframe-physics-component](https://github.com/ngokevin/aframe-physics-component)
-- [aframe-template-component](https://github.com/ngokevin/aframe-template-component)
-
---trim--
-## aframe-example-component
-
-A example component for [A-Frame](https://aframe.io).
-
-### API
+### Properties
 
 | Property | Description | Default Value |
 | -------- | ----------- | ------------- |
-|          |             |               |
+|    src      |    ChartBuilder json location         |   data.json          |
 
-### Installation
+### Usage
 
-#### Browser
+#### Properties
+
+The src property requires a path to a json file exported by ChartBuilder. It can be
+local or remote.
+
+#### Browser Installation
 
 Install and use by directly including the [browser files](dist):
 
@@ -41,27 +25,27 @@ Install and use by directly including the [browser files](dist):
 <head>
   <title>My A-Frame Scene</title>
   <script src="https://aframe.io/releases/0.2.0/aframe.min.js"></script>
-  <script src="https://rawgit.com/ngokevin/aframe-component-boilerplate/master/dist/aframe-example-component.min.js"></script>
+  <script src="https://rawgit.com/impronunciable/aframe-chartbuilder-component/master/dist/aframe-chartbuilder-component.min.js"></script>
 </head>
 
 <body>
   <a-scene>
-    <a-entity example="exampleProp: exampleVal"></a-entity>
+    <a-entity chartbuilder="src: data.json"></a-entity>
   </a-scene>
 </body>
 ```
 
-#### npm
+#### NPM Installation
 
-Install via npm:
+Install via NPM:
 
 ```bash
-npm install aframe-example-component
+npm install aframe-chartbuilder-component
 ```
 
 Then register and use.
 
 ```js
 require('aframe');
-require('aframe-example-component');
+require('aframe-chartbuilder-component');
 ```
--- index.js
+++ index.js
@@ -1,51 +1,147 @@
-/* global AFRAME */
 
-if (typeof AFRAME === 'undefined') {
-  throw new Error('Component attempted to register before AFRAME was available.');
-}
-
-/**
- * Example component for A-Frame.
- */
-AFRAME.registerComponent('example', {
-  schema: { },
+  /* Module dependencies */
+  require('aframe-meshline-component');
+  require('aframe-text-component');
+  var d3 = require('d3');
 
   /**
-   * Set if component needs multiple instancing.
+   * Constant
    */
-  multiple: false,
+
+var COLOR_PALETTE = ['#a50026', '#fdae61', '#d73027', '#abd9e9', '#f46d43', '#74add1', '#4575b4', '#313695', '#999', '#666', '#ccc'];
+var margin = .5;
+
+
+  /* global AFRAME */
+
+  if (typeof AFRAME === 'undefined') {
+    throw new Error('Component attempted to register before AFRAME was available.');
+  }
 
   /**
-   * Called once when component is attached. Generally for initial setup.
+   * Chart component for A-Frame.
    */
-  init: function () { },
+  AFRAME.registerComponent('chartbuilder', {
+    schema: {
+      src: { default: '' }
+    },
 
-  /**
-   * Called when component is attached and when component data changes.
-   * Generally modifies the entity based on the data.
-   */
-  update: function (oldData) { },
+    /**
+     * Set if component needs multiple instancing.
+     */
+    multiple: false,
 
-  /**
-   * Called when a component is removed (e.g., via removeAttribute).
-   * Generally undoes all modifications to the entity.
-   */
-  remove: function () { },
+    /**
+     * Called once when component is attached. Generally for initial setup.
+     */
+    init: function () { },
 
-  /**
-   * Called on each scene tick.
-   */
-  // tick: function (t) { },
+    /**
+     * Called when component is attached and when component data changes.
+     * Generally modifies the entity based on the data.
+     */
+    update: function () {
+      var src = this.data.src;
+      d3.json(src, this.buildChart.bind(this));
+    },
 
-  /**
-   * Called when entity pauses.
-   * Use to stop or remove any dynamic or background behavior such as events.
-   */
-  pause: function () { },
+    /**
+     * Called when a component is removed (e.g., via removeAttribute).
+     * Generally undoes all modifications to the entity.
+     */
+    remove: function () { },
 
-  /**
-   * Called when entity resumes.
-   * Use to continue or add any dynamic or background behavior such as events.
-   */
-  play: function () { }
-});
+    /**
+     * Called on each scene tick.
+     */
+    // tick: function (t) { },
+
+    /**
+     * Called when entity pauses.
+     * Use to stop or remove any dynamic or background behavior such as events.
+     */
+    pause: function () { },
+
+    /**
+     * Called when entity resumes.
+     * Use to continue or add any dynamic or background behavior such as events.
+     */
+    play: function () { },
+
+    /**
+     * Builds the chart using the chartbuilder json
+     */
+    buildChart: function (data) {
+      this.chartData = data.chartProps;
+      var container = this.container = d3.select(this.el);
+      var width = this.width = 10;
+      var height = this.height = 10;
+      var yScale = this.yScale = d3.scaleLinear()
+        .domain(this.chartData.scale.primaryScale.domain)
+        .range([0, this.height]);
+
+      var xScale = this.xScale = d3.scaleLinear()
+        .domain([0, this.chartData.data[0].values.length])
+        .range([0, this.width]);
+
+      // Y axis
+      container.append('a-entity')
+      .attr('meshline', 'lineWidth: 2; path: 0 0 0, 0 ' + (this.height + margin) + ' 0;'
+        + ' color: #ccc;')
+        .selectAll('a-entity').data(this.chartData.scale.primaryScale.tickValues)
+        .enter().append('a-entity')
+          .attr('material', 'color: #333')
+          .attr('text', function(d) { return 'size: 0.2; text: ' + d; })
+          .attr('position', function(d, i) { return '-.6 ' + yScale(d) + ' 0' });
+
+      // X axis
+      container.append('a-entity')
+      .attr('meshline', 'lineWidth: 2; path: 0 0 0, ' + (this.width + margin) + ' 0 0;'
+        + ' color: #ccc;')
+
+      var self = this;
+      this.chartData.chartSettings.forEach(function(d, i) {
+        switch (d.type) {
+        case 'line':
+          self.buildLine(i);
+          break;
+        default:
+          self.buildBar(i);
+        }
+      });
+
+    },
+
+    buildLine: function (i) {
+      var data = this.chartData.data;
+      var settings = this.chartData.chartSettings[i];
+      this.values = data[i].values.map(function(d) { return d.value; });
+      var xScale = this.xScale, yScale = this.yScale;
+      var container = this.container;
+
+      var line = container.append('a-entity')
+      .attr('meshline', 'path: ' + this.values.map(function(d, i) {
+        return [xScale(i) + margin, yScale(d) + margin, 0].join(' ');
+      }).join(', ') + '; lineWidth: 4; color: ' + COLOR_PALETTE[settings.colorIndex])
+    },
+
+    buildBar: function (i) {
+      var data = this.chartData.data;
+      var container = this.container;
+      var xScale = this.xScale, yScale = this.yScale;
+      this.values = data[i].values.map(function(d) { return d.value; });
+
+      var bars = container.selectAll('a-box.bar').data(this.values);
+      bars.enter().append('a-box')
+      .classed('bar', true)
+      .attr('position', function (d, i) {
+        return [xScale(i) + margin, (yScale(d) / 2) + margin, 0].join(' ');
+      })
+      .attr('depth', .2)
+      .attr('width', this.width / this.values.length)
+      .attr('opacity', .5)
+      .attr('height', function (d) { return yScale(d); })
+      .attr('color', COLOR_PALETTE[i]);
+    }
+
+  });
